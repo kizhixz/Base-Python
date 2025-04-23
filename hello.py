2e01a3f1d4c6a88651d2cdcf555f4d20ffa7a938
kizhixz <kizhixz@gmail.com>
2025-04-23 17:20:17 -0300

    "kilux/0.1"

--- hello.py
+++ hello.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+"""
+Hello World Multi Linguas.
+
+Dependendo da lingua configurada no
+ambiente o programa exibe a mensage
+correspondente.
+"""
 
 # Este programa imprime Hello World
 print("Hello, World!")
